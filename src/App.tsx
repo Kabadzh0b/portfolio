80c213fc4065136bac69379a26a65acb90bbd569
Kabadzh0b <chubbudda@gmail.com>
2023-07-24 13:29:02 +0300

    portfolio base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// @ts-ignore
+import avatar from './images/avatar.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="NavBar">
+                <button className="NavBarItem">Logo</button>
+                <div className="NavBarMenu">
+                    <button className="NavBarItem">My projects</button>
+                    <button className="NavBarItem">Say Hello</button>
+                </div>
+            </div>
+            <section className="Hero">
+                <div className="HeroHeader">
+                    <h1>Frontend Developer</h1>
+                    <p>I design and code beautifully simple things, and I love what I do.</p>
+                </div>
+                <img src={avatar} alt="my avatar"/>
+            </section>
+        </div>
+    );
 }
 
 export default App;
