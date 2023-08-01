897d2e020abb57c015210cd8c2926f77dd7c2afc
Kabadzh0b <chubbudda@gmail.com>
2023-08-01 13:33:19 +0300

    text changed

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -8,10 +8,9 @@ const Information = () => {
         <div className="BackGroundDarkBlue">
             <img className="Art" src={art} alt="cute art"/>
             <h1>Hi, I’m Max. Nice to meet you.</h1>
-            <p>Since beginning my journey as a freelance designer over 11 years ago, I've done remote<br/>
-                work for agencies, consulted for startups, and collaborated with talented people to create<br/>
-                digital products for both business and consumer use. I'm quietly confident, naturally<br/>
-                curious, and perpetually working on improving my chops one design problem at a time.<br/>
+            <p>Since beginning my journey as a developer over a year ago, I've done a lot of projects<br/>
+                for improving my skills, consulted with a strong developers, and collaborated with talented people.<br/>
+                I'm quietly confident, naturally curious, and perpetually working on improving my skills<br/>
                 I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
         </div>
     )
